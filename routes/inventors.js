ed7753bd4878cd2901f9aa96288894b76e0c7b21
vanessalevano2020 <lesvanell@gmail.com>
2020-10-14 23:38:27 -0300

    desafio JWT terminado

--- routes/inventors.js
+++ routes/inventors.js
@@ -11,12 +11,12 @@ router.get('/', verifyToken, async function (req, res, next) {
 
 // GET de un inventor
 // /inventors/56
-router.get('/:id', async (req, res) => {
+router.get('/:id', verifyToken, async (req, res) => {
   res.json(await dataInventor.getInventor(req.params.id));
 });
 
 // POST alta de un inventor
-router.post('/', async (req, res) => {
+router.post('/', verifyToken, async (req, res) => {
   const inventor = req.body;
   await dataInventor.pushInventor(inventor);
   const inventorPersistido = await dataInventor.getInventor(inventor._id);
@@ -24,7 +24,7 @@ router.post('/', async (req, res) => {
 });
 
 // PUT modificacion de un inventor
-router.put('/:id', async (req, res) => {
+router.put('/:id', verifyToken, async (req, res) => {
   const inventor = req.body;
   inventor._id = req.params.id;
   await dataInventor.updateInventor(inventor);
@@ -32,7 +32,7 @@ router.put('/:id', async (req, res) => {
   res.json(await dataInventor.getInventor(req.params.id));
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', verifyToken, async (req, res) => {
   await dataInventor.deleteInventor(req.params.id);
   res.send('Inventor eliminado');
 });
